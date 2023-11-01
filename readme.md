329688f2bd8b4660d119a75d155b61d47cfc1344
Shafi <shfahmd001@gamil.com>
2023-11-01 17:57:29 +0530

    html, css and js files along with readme file are added

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Online calculator
+
+
+using HTML, CSS and JS
---
+++ script.js
@@ -0,0 +1,21 @@
+let str = "";
+
+let buttons = document.querySelectorAll('.btn');
+Array.from(buttons).forEach((temp) => {
+temp.addEventListener('click', (e) => {
+if(e.target.innerHTML === '='){
+    str = eval(str);
+    document.querySelector('input').value = str;
+}
+else if(e.target.innerHTML === 'C'){
+    str = "";
+    document.querySelector('input').value = str;
+}
+else{
+console.log(e.target);
+str = str + e.target.innerHTML;
+document.querySelector('input').value = str;
+}
+
+})
+})
